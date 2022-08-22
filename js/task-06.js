48f6eaa4df0bf82805aa789a6cc60dab80df8044
vl00b <110897682+Vl00B@users.noreply.github.com>
2022-08-22 22:10:01 +0300

    .trim()

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,9 @@ const input = document.querySelector('#validation-input');
 const requiredLength = input.dataset.length;
 
 input.addEventListener('blur', (event) => {
+  let checker = event.currentTarget.value.trim();
+  event.currentTarget.value = checker;
+
   if (event.currentTarget.value.length == 6) {
     input.classList.add('valid');
     input.classList.remove('invalid');
